fe5a6a04edb34b60f5bc774bf160ba3d72dc1299
Koki <u2daijibrother@gmail.com>
2015-02-09 06:45:40 +0900

    first commit;

---
+++ SwapWithoutTempValue.Test/SwapTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SwapWithoutTempValue.Test
+{
+    [TestFixture]
+    public class SwapTest
+    {
+        [Test]
+        public void Swap2_should_swap_arg_src_to_dest()
+        {
+            // Arrange
+            int src = 0;
+            int dest = 10;
+
+            // Act
+            SwapService.Swap(ref src, ref dest);
+
+            // Assert
+            Assert.AreEqual(10, src);
+            Assert.AreEqual(0, dest);
+
+        }
+
+        [Test, TestCaseSource("TestData")]
+        public void Swap2_should_(int src, int dest)
+        {
+            int org_src = src;
+            int org_dest = dest;
+            SwapService.Swap(ref src, ref dest);
+            Assert.AreEqual(org_src, dest);
+            Assert.AreEqual(org_dest, src);
+        }
+
+        static object[] TestData =
+        {
+            new object[] { 12, 3 },
+            new object[] { 12, 2 },
+            new object[] { 12, 4 },
+            new object[] { 56666, 877 },
+            new object[] { 9938383, 787878 },
+            new object[] { 0, -88 },
+            new object[] { -8, 0 },
+            new object[] { 0, 0 },
+            new object[] { -1, -1 },
+            new object[] { 1, 1 }
+        };
+    }
+}
---
+++ SwapWithoutTempValue/Swap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapWithoutTempValue
+{
+    public class SwapService
+    {
+        public static void Swap(ref int src, ref int dest)
+        {
+            src = src^dest;
+            dest = src^dest;
+            src = src^dest;
+        }
+    }
+}
